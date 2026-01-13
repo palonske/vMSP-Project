ea74cc3d295e77c36346ce1c6966b77480b6946e
ericpalonsky <ericcary@gmail.com>
2026-01-13 15:41:00 -0800

    setting up the directories and file structure needed.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+from xml.etree.ElementTree import tostring
+
+from fastapi import FastAPI
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    message = "Hello World"
+    log(message)
+    return {"message": message}
+
 def main():
     print("Hello from emspprojectv1!")
 
+def log(string):
+    now_utc = datetime.now(ZoneInfo("UTC"))
+    logger = now_utc.strftime("%Y-%m-%d %H:%M:%S") + ":  Sending API Response:  " + string
+    print(logger)
 
 if __name__ == "__main__":
     main()
